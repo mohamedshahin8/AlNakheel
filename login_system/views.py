fffc00359e42a5206202cb9cdd415c93ff95cb2d
mohamed shahin <45289916+mohamedshahin8@users.noreply.github.com>
2022-06-30 22:41:31 +0200

    add files

---
+++ login_system/views.py
@@ -0,0 +1,34 @@
+
+# Create your views here.
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Invalid Username or Password')
+            return redirect('login_user')
+
+
+    else:
+        return render(request, 'login.html')
+
+
+
+def logout_user(request):
+    auth.logout(request)
+    return redirect('home')
