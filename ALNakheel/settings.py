0200c330c7eaddd426b963254374ae6d329913b0
mohamed shahin <45289916+mohamedshahin8@users.noreply.github.com>
2022-07-02 17:58:54 +0200

    directory changed

--- ALNakheel/settings.py
+++ ALNakheel/settings.py
@@ -17,6 +17,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
