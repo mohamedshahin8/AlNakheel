83a583fec5bf714a5b3fb4bd743283752b2613df
mohamed shahin <45289916+mohamedshahin8@users.noreply.github.com>
2022-07-02 15:50:43 +0200

    initail commit

--- ALNakheel/urls.py
+++ ALNakheel/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
+from login_system.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('login_system.urls')),
 ]
+
+# urlpatterns += [
+#     path('/', include('login_system.urls')),
+# ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
--- login_system/views.py
+++ login_system/views.py
@@ -3,8 +3,10 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
+from django.contrib.auth.decorators import login_required
 
 
+# @login_required
 def home(request):
     return render(request, 'home.html')
 
